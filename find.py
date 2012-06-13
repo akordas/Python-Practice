1ef0efe68c587e2ce53550ed635263c7da2400d7
akordas <alexjkordas@gmail.com>
2012-06-13 10:26:52 -0500

    Got Regex working, trying to get globbing working now

--- find.py
+++ find.py
@@ -7,8 +7,7 @@ parser = argparse.ArgumentParser(description='Process commandline options')
 
 parser.add_argument("path", help="the path that you wish to search")
 
-parser.add_argument("-regex", help="using regular expressions",
-			action="store_true")
+parser.add_argument("-regex", help="using regular expressions")
 
 parser.add_argument("-name", help="search for an exact name")
 
@@ -16,11 +15,28 @@ parser.add_argument("-type", help="searching for a specific type of file")
 
 args = parser.parse_args()
 
-for (root, dirs, filenames) in os.walk(args.path):
-	for filename in filenames:
-		if args.name == filename:
-			#if we find the key in the filename
-			print os.path.join(root, filename)
-	for dirname in dirs:
-		if args.name == dirname:
-			print os.path.join(root, dirname)
+if args.name:
+	for (root, dirs, filenames) in os.walk(args.path):
+		for dirt in dirs:
+			for blah in glob.glob(args.name):
+				print blah
+"""
+working code that finds exact file names. Excluded for testing globbing
+	for (root, dirs, filenames) in os.walk(args.path):
+		for filename in filenames:
+			if args.name == filename:
+				#if we find the key in the filename
+				print os.path.join(root, filename)
+		for dirname in dirs:
+			if args.name == dirname:
+				print os.path.join(root, dirname)
+"""
+if args.regex:
+	pattern = re.compile(args.regex)
+	for (root, dirs, filenames) in os.walk(args.path):
+		for filename in filenames:
+			if re.match(pattern, filename):
+				print os.path.join(root, filename)
+		for dirname in dirs:
+			if re.match(pattern, dirname):
+				print os.path.join(root, dirname)
