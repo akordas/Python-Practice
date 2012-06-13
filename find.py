45f6aaf1f5bd99699ce6758af8ebed6d76381fbc
akordas <alexjkordas@gmail.com>
2012-06-13 11:54:20 -0500

    Added some error checking and started an experimental section for glob recursion

--- find.py
+++ find.py
@@ -1,9 +1,11 @@
-import os
-import argparse
-import glob
-import re
+from os import walk
+from os.path import relpath, join
+from argparse import ArgumentParser
+from glob import glob
+from re import compile, match
+from sys import exit
 
-parser = argparse.ArgumentParser(description='Process commandline options')
+parser = ArgumentParser(description='Process commandline options')
 
 parser.add_argument("path", help="the path that you wish to search")
 
@@ -15,27 +17,42 @@ parser.add_argument("-type", help="searching for a specific type of file")
 
 args = parser.parse_args()
 
+if args.name and args.regex:
+	print "Please pass in only a globbed name or a regex expression"
+	exit()
+
+if not args.name and not args.regex:
+	print "Please provide either a globbed name or a regex expression"
+	exit()
+
 if args.name:
-	wheretolook = args.path + os.path.relpath(args.name)
-	for blah in glob.glob(wheretolook):
+	wheretolook = args.path + relpath(args.name)
+	for blah in glob(wheretolook):
 		print blah
-"""
-working code that finds exact file names. Excluded for testing globbing
-	for (root, dirs, filenames) in os.walk(args.path):
+
+	print "entering the experimental zone now. brace yourself."
+	for (root, dirs, filenames) in walk(args.path):
+		for dirname in dirs:
+			wheretolook = relpath(dirname)+relpath(args.name, dirname)
+			for blah in glob(wheretolook):
+				print blah
+
+	print "this is what the exact-name search would yield"
+	for (root, dirs, filenames) in walk(args.path):
 		for filename in filenames:
 			if args.name == filename:
 				#if we find the key in the filename
-				print os.path.join(root, filename)
+				print join(root, filename)
 		for dirname in dirs:
 			if args.name == dirname:
-				print os.path.join(root, dirname)
-"""
+				print join(root, dirname)
+
 if args.regex:
-	pattern = re.compile(args.regex)
-	for (root, dirs, filenames) in os.walk(args.path):
+	pattern = compile(args.regex)
+	for (root, dirs, filenames) in walk(args.path):
 		for filename in filenames:
-			if re.match(pattern, filename):
-				print os.path.join(root, filename)
+			if match(pattern, filename):
+				print join(root, filename)
 		for dirname in dirs:
-			if re.match(pattern, dirname):
-				print os.path.join(root, dirname)
+			if match(pattern, dirname):
+				print join(root, dirname)
