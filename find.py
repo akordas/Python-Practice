bdf80669367d3a1785244a6339c00fc64e18790b
akordas <alexjkordas@gmail.com>
2012-06-15 11:01:28 -0500

    Now using indexing for exact searches

--- find.py
+++ find.py
@@ -1,9 +1,10 @@
 from os import walk
-from os.path import relpath, join
+from os.path import relpath, join, exists
 from argparse import ArgumentParser
 from re import compile, match
 from sys import exit
 from fnmatch import filter, fnmatch
+import anydbm
 
 parser = ArgumentParser(description='Process commandline options')
 
@@ -43,21 +44,35 @@ if args.n:
 if args.e:
 	###this is the code for searching for the exact filename
 	for (root, dirs, filenames) in walk(args.path):
-		for filename in filenames:
-			if args.n == filename:
-				#if we find the key in the filename
-				if not args.t:
-					print join(root, filename)
-				else:
-					if args.t == 'f':
+		if exists(join(root, "fsys.idx")):
+			index = anydbm.open(join(root, "fsys.idx"), 'r')
+			try:
+				if  index[args.e]:
+					if not args.t:
+						print join(root, args.e)
+					elif args.t == 'd' and index[args.e]=='d':
+						print join(root, args.e)
+					elif args.t == 'f' and index[args.e]=='f':
+						print join(root, args.e)
+			except:
+				pass
+			index.close()
+		else:
+			for filename in filenames:
+				if args.n == filename:
+					#if we find the key in the filename
+					if not args.t:
 						print join(root, filename)
-		for dirname in dirs:
-			if args.n == dirname:
-				if not args.t:
-					print join(root, dirname)
-				else:
-					if args.t == 'd':
+					else:
+						if args.t == 'f':
+							print join(root, filename)
+			for dirname in dirs:
+				if args.n == dirname:
+					if not args.t:
 						print join(root, dirname)
+					else:
+						if args.t == 'd':
+							print join(root, dirname)
 
 if args.r:
 	pattern = compile(args.r)
